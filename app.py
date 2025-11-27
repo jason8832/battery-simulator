5e25a30b2285adb1232be23b924869c9ac145739
jason8832 <jason8832@ajou.ac.kr>
2025-11-27 23:31:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -352,8 +352,8 @@ with tab3:
         with st.container(border=True):
             st.markdown("#### 🛠️ 공정 조건 설정 (음극)")
             # [음극용 바인더 순서 변경: 수계가 메인이므로 SBR, CMC 등을 앞으로]
-            s_binder = st.selectbox("Binder Type", ["SBR", "CMC", "CMGG", "GG", "PVDF"]) 
-            s_solvent = st.radio("Solvent Type", ["Water", "NMP"])
+            s_binder = st.selectbox("Binder Type", ["PVDF","CMGG", "GG","CMC"]) 
+            s_solvent = st.radio("Solvent Type", ["NMP","Water"])
             st.divider()
             s_temp = st.slider("Drying Temp (°C)", 60, 200, 110)
             s_time = st.slider("Drying Time (min)", 10, 720, 60) 
